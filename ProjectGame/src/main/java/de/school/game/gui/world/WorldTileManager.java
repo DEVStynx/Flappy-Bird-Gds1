93a013831a7902899c2a49ac5721c21085f94461
Jonas <131193709+DEVStynx@users.noreply.github.com>
2025-03-25 12:27:51 +0100

    Update WorldTileManager.java

--- ProjectGame/src/main/java/de/school/game/gui/world/WorldTileManager.java
+++ ProjectGame/src/main/java/de/school/game/gui/world/WorldTileManager.java
@@ -12,33 +12,46 @@ import java.util.HashMap;
 
 public class WorldTileManager {
     public HashMap<Integer, Tile> tileLibrary;
-    public HashMap<Integer,Tile> detailLibrary;
-    public int[][] loadedMap;
-    public int[][] loadedDetails;
+    public HashMap<Integer, Tile> detailLibrary;
+    public CollisionTileManager collisionTileManager;
+
     public int currentTileKey = 0;
     public int currentDetailKey = 0;
+    public int[][] loadedMap;
 
+    public int[][] loadedDetails;
+
+    //Todo: Redondante Methoden wie loadMapTextures in eine Methode fassen(loadfromfile(arr,src))
     public WorldTileManager() {
         tileLibrary = new HashMap<>();
         detailLibrary = new HashMap<>();
         loadedMap = new int[Game.gameWindow().maxScreenCol][Game.gameWindow().maxScreenRows];
         loadedDetails = new int[Game.gameWindow().maxScreenCol][Game.gameWindow().maxScreenRows];
-
-        addTileToLibrary(new Tile(RenderUtil.loadTexture(FileUtil.getFileByResource("textures/map/tiles/grass01.png"))));
-        addTileToLibrary(new Tile(RenderUtil.loadTexture(FileUtil.getFileByResource("textures/map/tiles/floor01.png"))));
-
+        collisionTileManager = new CollisionTileManager();
+        addTileToLibrary(new Tile(RenderUtil.loadTexture(FileUtil.getFileByResource("textures/map/tiles/tile017.png"))));
+        addTileToLibrary(new Tile(RenderUtil.loadTexture(FileUtil.getFileByResource("textures/map/tiles/tile021.png"))));
+        addTileToLibrary(new Tile(RenderUtil.loadTexture(FileUtil.getFileByResource("textures/map/tiles/tile012.png"))));
         addDetailToLibrary(new Tile(RenderUtil.loadTexture(FileUtil.getFileByResource("textures/map/details/air.png"))));
         addDetailToLibrary(new Tile(RenderUtil.loadTexture(FileUtil.getFileByResource("textures/map/details/tree01.png"))));
     }
+
     private void addTileToLibrary(Tile tile) {
-        tileLibrary.put(currentTileKey,tile);
+        tileLibrary.put(currentTileKey, tile);
         currentTileKey++;
     }
+
     private void addDetailToLibrary(Tile tile) {
-        detailLibrary.put(currentDetailKey,tile);
+        detailLibrary.put(currentDetailKey, tile);
         currentDetailKey++;
     }
-    public void loadMap(String resource) {
+
+    public void loadmap(String resource) {
+        loadMaptextures(resource + ".map");
+        loadDetails(resource + ".detail");
+        collisionTileManager.loadMapCollisions(resource + ".collision");
+    }
+
+    public void loadMaptextures(String resource) {
         try {
             InputStream stream = WorldTileManager.class.getResourceAsStream(resource);
             BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
@@ -64,7 +77,8 @@ public class WorldTileManager {
             e.printStackTrace();
         }
     }
-    public void loadDetaills(String resource) {
+
+    public void loadDetails(String resource) {
         try {
             InputStream stream = WorldTileManager.class.getResourceAsStream(resource);
             BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
@@ -90,7 +104,9 @@ public class WorldTileManager {
             e.printStackTrace();
         }
     }
-    public void render (Graphics2D graphics2D) {
+
+
+    public void render(Graphics2D graphics2D) {
         render(graphics2D, loadedMap, tileLibrary);
         render(graphics2D, loadedDetails, detailLibrary);
     }
@@ -101,10 +117,10 @@ public class WorldTileManager {
         int x = 0;
         int y = 0;
 
-        while (col <Game.gameWindow().maxScreenCol && row < Game.gameWindow().maxScreenRows) {
+        while (col < Game.gameWindow().maxScreenCol && row < Game.gameWindow().maxScreenRows) {
             graphics2D.drawImage(tiles.get(map[col][row]).texture, x, y, Game.gameWindow().tileSize, Game.gameWindow().tileSize, null);
             col++;
-            x+= Game.gameWindow().tileSize;
+            x += Game.gameWindow().tileSize;
             if (col == Game.gameWindow().maxScreenCol) {
                 col = 0;
                 x = 0;
@@ -114,5 +130,46 @@ public class WorldTileManager {
         }
     }
 
+    public class CollisionTileManager {
+        public int[][] WorldCollision;
 
+        public CollisionTileManager() {
+            WorldCollision = new int[Game.gameWindow().maxScreenCol][Game.gameWindow().maxScreenRows];
+            //0 ist keine Kollision
+            //1 ist die Kollision mit einer Wand
+            //2 ist das "sterben"/"neu beginnen" eines Levels
+        }
+
+        public void loadMapCollisions(String resource) {
+            try {
+                InputStream stream = WorldTileManager.class.getResourceAsStream(resource);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+                int row = 0;
+                while (row < Game.gameWindow().maxScreenRows) {
+                    String currentLine = reader.readLine();
+                    if (currentLine == null) break;
+
+                    String[] tileIds = currentLine.split(" ");
+                    for (int col = 0; col < Game.gameWindow().maxScreenCol; col++) {
+                        if (col < tileIds.length) {
+                            int coll = Integer.parseInt(tileIds[col]);
+                            if (coll > 2) {
+                                throw new IllegalArgumentException("diese Zahl/Kollision existiert nicht!");
+                            }
+                            WorldCollision[col][row] = coll;
+                        } else {
+                            WorldCollision[col][row] = 0;
+                        }
+                    }
+                    row++;
+                }
+                reader.close();
+                stream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
