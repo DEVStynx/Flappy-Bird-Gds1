c909d8341ba271753ba847b47db9d5b5380dadf7
Jonas <131193709+DEVStynx@users.noreply.github.com>
2025-03-25 12:28:31 +0100

    Update Game.java

--- ProjectGame/src/main/java/de/school/game/Game.java
+++ ProjectGame/src/main/java/de/school/game/Game.java
@@ -7,7 +7,6 @@ import de.school.game.gui.world.WorldTileManager;
 import de.school.game.input.InputListener;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Game extends JFrame {
     private static GameWindow gameWindow;
@@ -28,20 +27,28 @@ public class Game extends JFrame {
         inputListener = new InputListener();
         this.addKeyListener(inputListener);
         this.addMouseListener(inputListener);
-        player = new PlayerEntity(0,0,1);
+        player = new PlayerEntity(0, 0, 1);
         gameClock = new GameClock(FPS);
         gameClock.startGameThread();
         worldTileManager = new WorldTileManager();
-        worldTileManager.loadMap("/maps/map0.map");
-        worldTileManager.loadDetaills("/maps/map0.detail");
+        worldTileManager().loadmap("/maps/map0");
         this.setVisible(true);
     }
 
     public static GameWindow gameWindow() {
         return gameWindow;
     }
-    public static GameClock gameClock() {return gameClock;}
-    public static PlayerEntity player() {return player;}
-    public static WorldTileManager worldTileManager() {return worldTileManager;}
+
+    public static GameClock gameClock() {
+        return gameClock;
+    }
+
+    public static PlayerEntity player() {
+        return player;
+    }
+
+    public static WorldTileManager worldTileManager() {
+        return worldTileManager;
+    }
 
 }
